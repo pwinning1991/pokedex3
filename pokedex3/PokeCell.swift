715a0e8b5069daaab24c70bcc401039a812d952a
Philip Winnington <pwinnington@PhilsMacbookAir.lan>
2017-08-14 14:16:01 -0400

    added files and setup collection view and csv parser

---
+++ pokedex3/PokeCell.swift
@@ -0,0 +1,13 @@
+//
+//  PokeCell.swift
+//  pokedex3
+//
+//  Created by Philip Winnington on 8/14/17.
+//  Copyright © 2017 Philip Winnington. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+}
---
+++ pokedex3/pokemon.swift
@@ -0,0 +1,9 @@
+//
+//  pokemon.swift
+//  pokedex3
+//
+//  Created by Philip Winnington on 8/10/17.
+//  Copyright © 2017 Philip Winnington. All rights reserved.
+//
+
+import Foundation
